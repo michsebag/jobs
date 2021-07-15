c87cff71c987225dc55cbf28dc4a1d13a6df8f44
michaelseb <michael.sebag@nice.com>
2021-07-15 16:18:53 +0300

    Pairing interview

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -1,6 +1,6 @@
 import {RequestHandler} from "express";
 import got from "got";
-import {minSatisfying} from "semver";
+import {maxSatisfying} from "semver";
 import {NPMPackage} from "./types";
 import {IDependency} from "./interfaces/IDependency";
 
@@ -8,6 +8,8 @@ import {IDependency} from "./interfaces/IDependency";
  * Attempts to retrieve package data from the npm registry and return it
  */
 const npmUrl = 'https://registry.npmjs.org';
+const cacheMap = new Map();
+
 
 export const getPackage: RequestHandler = async function (req, res, next) {
     try {
@@ -23,7 +25,7 @@ async function getPackageDependenciesTree(res, req) {
     const npmPackage: NPMPackage = await got(
         `${npmUrl}/${name}`
     ).json();
-    dependencyTree = await generateDependenciesTree(npmPackage, version)
+    dependencyTree = await generateDependenciesTree(npmPackage, version, name)
     return res
         .status(200)
         .json({
@@ -34,27 +36,35 @@ async function getPackageDependenciesTree(res, req) {
 }
 
 
-async function generateDependenciesTree(npmPackage, version): Promise<IDependency[]> {
+async function generateDependenciesTree(npmPackage, version, path): Promise<IDependency[]> {
     const newDeps = npmPackage.versions[version].dependencies;
     let packageDependencies = Object.entries(newDeps || ({} as Object));
     const dependencyTree: IDependency[] = [];
     for (const [dependencyName, version] of packageDependencies) { // extract for
-        const subDep = await getDependencies(dependencyName, version);
-        const currentDependency: IDependency =  {
-            name: dependencyName,
-            version: version,
-            dependencies: subDep
-        };
-        dependencyTree.push(currentDependency)
+        if(path.contains(dependencyName)){
+            continue;
+        }
+        if(cacheMap.has(dependencyName)){
+            dependencyTree.push(cacheMap.get(dependencyName))
+        }else{
+            const subDep = await getDependencies(dependencyName, version, `${path}/${dependencyName}`);
+            const currentDependency: IDependency =  {
+                name: dependencyName,
+                version: version,
+                dependencies: subDep
+            };
+            dependencyTree.push(currentDependency)
+            cacheMap.set(dependencyName, currentDependency);
+        }
     }
     return dependencyTree
 }
 
-async function getDependencies(name, version) : Promise<IDependency[]> {
+async function getDependencies(name, version, path) : Promise<IDependency[]> {
     return got(`${npmUrl}/${name}`)
         .json()
         .then((npmPackage: any) => {
-            let lowestVersion = minSatisfying(Object.keys(npmPackage.versions), version);
-            return lowestVersion ? generateDependenciesTree(npmPackage, lowestVersion) : [];
+            let maximalVersion = maxSatisfying(Object.keys(npmPackage.versions), version);
+            return (maximalVersion && !path.contains(name)) ? generateDependenciesTree(npmPackage, maximalVersion, path) : [];
         });
 }
