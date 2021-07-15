03c1e164aabee31caa47eab1383160073ace5a49
michaelseb <michael.sebag@nice.com>
2021-07-15 15:27:33 +0300

    Michael Changes

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -1,53 +1,60 @@
-import { RequestHandler } from "express";
+import {RequestHandler} from "express";
 import got from "got";
-import { minSatisfying } from "semver";
-import { NPMPackage } from "./types";
+import {minSatisfying} from "semver";
+import {NPMPackage} from "./types";
+import {IDependency} from "./interfaces/IDependency";
 
 /**
  * Attempts to retrieve package data from the npm registry and return it
  */
+const npmUrl = 'https://registry.npmjs.org';
+
 export const getPackage: RequestHandler = async function (req, res, next) {
-  const { name, version } = req.params;
-  const dependencyTree = {};
-  try {
-    const npmPackage: NPMPackage = await got(
-      `https://registry.npmjs.org/${name}`
-    ).json();
-
-    const dependencies = npmPackage.versions[version].dependencies;
-
-    for (const [dep, version] of Object.entries(dependencies as Object)) {
-      const subDep = await getDependencies(dep, version);
-      dependencyTree[dep] = { version, dependecies: subDep };
+    try {
+        return await getPackageDependenciesTree(res, req)
+    } catch (error) {
+        return next(error);
     }
-
-    return res
-      .status(200)
-      .json({ name, version, dependencies: dependencyTree });
-  } catch (error) {
-    return next(error);
-  }
 };
 
-async function getDependencies(name, version) {
-  return got(`https://registry.npmjs.org/${name}`)
-    .json()
-    .then((npmPackage: any) => {
-      let v = minSatisfying(Object.keys(npmPackage.versions), version);
+async function getPackageDependenciesTree(res, req) {
+    const {name, version} = req.params;
+    let dependencyTree = {};
+    const npmPackage: NPMPackage = await got(
+        `${npmUrl}/${name}`
+    ).json();
+    dependencyTree = await generateDependenciesTree(npmPackage, version)
+    return res
+        .status(200)
+        .json({
+            name: name,
+            version: version,
+            dependencies: dependencyTree
+        });
+}
 
-      if (v) {
-        const dependencyTree = {};
-        const newDeps = npmPackage.versions[v].dependencies;
-        for (const [dep, version] of Object.entries(
-          newDeps || ({} as Object)
-        )) {
-          const subDep = getDependencies(dep, version);
-          dependencyTree[dep] = { version, dependecies: subDep };
-        }
 
-        return dependencyTree;
-      } else {
-        return {};
-      }
-    });
+async function generateDependenciesTree(npmPackage, version): Promise<IDependency[]> {
+    const newDeps = npmPackage.versions[version].dependencies;
+    let packageDependencies = Object.entries(newDeps || ({} as Object));
+    const dependencyTree: IDependency[] = [];
+    for (const [dependencyName, version] of packageDependencies) { // extract for
+        const subDep = await getDependencies(dependencyName, version);
+        const currentDependency: IDependency =  {
+            name: dependencyName,
+            version: version,
+            dependencies: subDep
+        };
+        dependencyTree.push(currentDependency)
+    }
+    return dependencyTree
+}
+
+async function getDependencies(name, version) : Promise<IDependency[]> {
+    return got(`${npmUrl}/${name}`)
+        .json()
+        .then((npmPackage: any) => {
+            let lowestVersion = minSatisfying(Object.keys(npmPackage.versions), version);
+            return lowestVersion ? generateDependenciesTree(npmPackage, lowestVersion) : [];
+        });
 }
